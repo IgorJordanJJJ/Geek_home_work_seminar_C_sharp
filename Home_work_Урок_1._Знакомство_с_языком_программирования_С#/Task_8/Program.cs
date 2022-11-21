6aa4d8dc30b1b612cd2896b8882cc7b745a36170
Igor <igorjordan210@gmail.com>
2022-11-22 00:20:41 +0300

    Add Task_8

---
+++ Home_work_Урок_1._Знакомство_с_языком_программирования_С#/Task_8/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Write("Write a number\n");
+int len = int.Parse(Console.ReadLine());
+int num = 1;
+if(len>0)
+{
+    while( num <= len)
+    {
+        if (num%2 == 0)
+        {
+        Console.Write($"{num} \t");
+        }
+        num++;
+    }
+}
+else if (len ==0 )
+{
+    Console.Write(0);
+}
+else 
+{
+    Console.Write("Dangerous");
+}
