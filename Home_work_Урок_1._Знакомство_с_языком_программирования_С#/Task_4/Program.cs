cb5d466a72d8f1c0fc699aab24ad835cc133c6c7
Igor <igorjordan210@gmail.com>
2022-11-21 23:59:17 +0300

    add Task_4

---
+++ Home_work_Урок_1._Знакомство_с_языком_программирования_С#/Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.Write("Write the number No. 1 \n ");
+double maxnumber = double.Parse(Console.ReadLine());
+double num;
+int count =0;
+while (count<2)
+{
+    Console.Write($"Write the number No. {count+2}\n ");
+    num = double.Parse(Console.ReadLine());
+    if(maxnumber<num)
+    {
+        maxnumber = num;
+    }
+    count++;
+}
+Console.Write($"Max number\t =\t {maxnumber}");
