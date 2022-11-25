f383c363c14f80f9622fbba13e8ab7b9746f3cfe
Igor <igorjordan210@gmail.com>
2022-11-26 01:41:35 +0300

    Add Task_10

---
+++ Home_work_Урок_2._Базовые_алгоритмы/Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Write number");
+int number = int.Parse(Console.ReadLine());
+number = number%100;
+number = number/10;
+Console.WriteLine(number);
