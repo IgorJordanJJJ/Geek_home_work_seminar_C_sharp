d22435e63c3b8b0cffd595e6209a5dede9696e03
Igor <igorjordan210@gmail.com>
2022-12-01 21:40:42 +0300

    Add Task27

---
+++ Home_work_Урок_4._Функции/Task_27/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+string GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    string test = "";
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        test = Console.ReadLine();
+        if(int.TryParse(test, out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    
+    return test;
+}
+
+int NumOfNum (string a)
+{
+    int num = 0;
+    int b = int.Parse(a);
+    for (int i = 1; i <= a.Length; i++)
+    {
+        num = num + b%10;
+        b = b/10;
+    }
+
+    return num;
+}
+
+string a = GetNumber("Введите число");
+Console.WriteLine($"Сумму цифр в числе {a} = {NumOfNum(a)}");
