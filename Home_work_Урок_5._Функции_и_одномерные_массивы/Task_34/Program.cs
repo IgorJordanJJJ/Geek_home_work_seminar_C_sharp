7d5068506c75d3cf271c3c6c1744c223c379304e
Igor <igorjordan210@gmail.com>
2022-12-07 21:57:51 +0300

    Add Task34

---
+++ Home_work_Урок_5._Функции_и_одномерные_массивы/Task_34/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+//инициализирует новый массив
+int[] InitArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(100,1000);
+    }
+
+    return array;
+}
+//печатает массив на экран
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+//получаем количество четных  чисел в массиве
+int GetPositivNumbres(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]%2 == 0)
+            count++;
+    }
+
+    return count;
+}
+int[] arr = InitArray(10);
+PrintArray(arr);
+int count = GetPositivNumbres(arr);
+Console.WriteLine($"Количество четных чсел = {count}");
