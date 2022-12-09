244816f92d1f45daf88088ef0319ddc701933b35
Igor <igorjordan210@gmail.com>
2022-12-09 20:27:12 +0300

    Add Task_45

---
+++ Home_work_Урок_6._Одномерные массивы. Продолжение/Task_45/Program.cs
@@ -0,0 +1,70 @@
+﻿/*Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.*/
+
+//метод, который будет выдавать заполненный массив случайными числами
+int[] InitArray(int dimension)
+{
+    int[] array = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(1,9);
+    }
+
+    return array;
+}
+
+//метод, который будет печатать массив
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+//получение числа с консоли, для того, чтобы задать размерность массива
+int GetNumberFromConsole(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Введите число");
+        }
+    }
+
+    return result;
+}
+// создаваём копию заданного массива с помощью поэлементного копирования
+int[] CopyArray(int[] art )
+{
+    int len = art.Length;
+    int[] array = new int[len];
+    
+
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = art[i];
+    }
+
+    return array;
+}
+int dimension = GetNumberFromConsole("Введите размерность массива");
+int[] startArray = InitArray(dimension);
+Console.WriteLine("Создан массив с элементами");
+PrintArray(startArray);
+int[] resultArray = CopyArray(startArray);
+Console.WriteLine("Создана копия массива с элементами");
+PrintArray(resultArray);
+PrintArray(resultArray);
